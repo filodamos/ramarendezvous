d86b8a86edef86879234fde0ca436122466b414b
Eleftheria Ekatommati <eleftheria.ekatommati@gmail.com>
2025-01-30 14:57:42 +0200

    making the timeline and the three boxes

--- src/components/ContentBoxes/index.tsx
+++ src/components/ContentBoxes/index.tsx
@@ -1,15 +1,36 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styles from './index.module.css'
-
 
 export function ContentBoxes() {
   return (
     <div className={styles.ContentBoxes}>
-        <div className={styles.box}>
-        <p> Hello user</p>
-        <p>Last login:9/06/2025 </p>
-        </div>
+      <div className={styles.timeline}>
+        <div className={styles.line}></div>
+        <div className={styles.timelinePoint}></div>
+        <div className={styles.timelinePoint}></div>
+        <div className={styles.timelinePoint}></div>
+      </div>
 
+      <div className={styles.box}>
+        <p>Hello User</p>
+        <p>Last login: 9/06/2025</p>
+      </div>
+
+      <div className={styles.box}>
+        <p>Completed Onboarding</p>
+        <p>10/06/2025</p>
+      </div>
+
+      <div className={styles.box}>
+        <p>Profile Updated</p>
+        <p>12/06/2025</p>
+      </div>
+      <div className={styles.news}>
+        <div className={styles.tab}></div>
+        <div className={styles.tab}></div>
+        <div className={styles.tab}></div>
+      </div>
     </div>
   )
 }
+
