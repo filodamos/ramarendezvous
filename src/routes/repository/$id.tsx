e057ee07cefa01a9b669734a3ac027e38c529f32
Eleftheria Ekatommati <eleftheria.ekatommati@gmail.com>
2025-02-13 14:53:59 +0200

    making the connection to the backend

--- src/routes/repository/$id.tsx
+++ src/routes/repository/$id.tsx
@@ -1,12 +1,42 @@
 import { createFileRoute, useParams } from '@tanstack/react-router'
 import styles from './index.module.css'
+import { useEffect, useState } from 'react';
 
 export const Route = createFileRoute('/repository/$id')({
   component: RouteComponent,
 })
 
+interface DataResponse {
+  message: string
+}
+
 function RouteComponent() {
   const { id } = useParams({ strict: false })
+ 
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+     console.time('fetchTime')
+        const response = await fetch(
+          'http://127.0.0.1:8000/repos/EleftheriaEkatommati'
+        )
+        console.timeLog('fetchTime', 'Received response')
+
+        // Check if the response is OK (status 200)
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`)
+        }
+
+        const json = await response.json()
+        setData(json)
+        console.timeEnd('fetchTime')
+
+    }
+
+    fetchData()
+  }, [])
+
   return (
     <div className={styles.Details}>
       <h1>Page {id}</h1>
@@ -15,6 +45,7 @@ function RouteComponent() {
         Some information about the repository , the creator , the project and
         the whole thing
       </p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   )
 }
